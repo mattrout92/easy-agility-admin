8ee6d7606e44c2010d0256a671b307c3b536ecba
mattrout92 <matt_rout92@live.co.uk>
2024-12-09 09:08:53 +0000

    christmas show

--- src/App.tsx
+++ src/App.tsx
@@ -59,8 +59,8 @@ type C = {
   status?: string;
 };
 
-const showID = 34;
-const ringId = 7;
+const showID = 35;
+const ringId = 3;
 
 function App() {
   const [faults, setFaults] = useState<string[]>([]);
