79d8e7da70222b19fe6b41c4e2dd6c2a03627397
mattrout92 <matt_rout92@live.co.uk>
2024-12-31 09:49:52 +0000

    babel jan 25

--- src/App.tsx
+++ src/App.tsx
@@ -59,8 +59,8 @@ type C = {
   status?: string;
 };
 
-const showID = 31;
-const ringId = 8;
+const showID = 34;
+const ringId = 7;
 
 function App() {
   const [faults, setFaults] = useState<string[]>([]);
