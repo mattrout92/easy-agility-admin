68795e2f685ea95a70b3a75a283f44b4eaee8f75
mattrout92 <matt_rout92@live.co.uk>
2025-01-12 06:36:19 +0000

    epynt show

--- src/App.tsx
+++ src/App.tsx
@@ -59,8 +59,8 @@ type C = {
   status?: string;
 };
 
-const showID = 34;
-const ringId = 7;
+const showID = 32;
+const ringId = 9;
 
 function App() {
   const [faults, setFaults] = useState<string[]>([]);
